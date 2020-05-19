39ca2ca1337c1eceb6f456072ca830102c512da6
sukolekar <surajkolekar123@gmail.com>
2020-05-19 19:35:38 +0530

    Test Case 2 FALSE here because year is 1700 we want it is not leap year but it show leap year

--- MainLeap.java
+++ MainLeap.java
@@ -1,6 +1,6 @@
 class MainLeap{
   public static void main(String args[]){
-    int year = 2000;
+    int year = 1600;
     TestCase tc = new TestCase();
     tc.test(year);
   }
--- TestCase.java
+++ TestCase.java
@@ -2,9 +2,19 @@ public class TestCase {
   public void test(int year){
 
       //Test Case 1:
-      if(year%400==0){
-        System.out.println("Leap Year");
+      // if(year%400==0){
+      //   System.out.println("Leap Year");
+      // }
+
+      //Test Case 2: Leap year % 100 or Leap %400 !=0
+      if(year%100==0){
+        if(year%400!=0){
+          System.out.println("This "+year+" is Not leap year");
+        }else{
+          System.out.println("this "+year+ " is Leap year");
+        }
       }
+
   }
   
-}
+} 
